4fb854b1dca33c05e622f67a7747312c86523f92
intern laptop <justin.c.kirk@gmail.com>
2023-03-12 11:13:13 -0400

    back up before frontdev branch

--- src/pages/library/index.tsx
+++ src/pages/library/index.tsx
@@ -99,7 +99,7 @@ export default function LibraryPage() {
             return (
               <div key={book.id} className='library-book-div'>
                 <h2>{book.title}</h2>
-                <h4>{book.authors ? book.authors[0] : ''}</h4>
+                {/* <h4>{book.authors ? book.authors[0] : ' '}</h4> */}
               </div>
             )
           })
