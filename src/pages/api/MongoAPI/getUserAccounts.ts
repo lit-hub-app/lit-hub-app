b515bf90fc9e5d9659fdb7a5e60c17a242466570
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-03-15 17:08:52 -0400

    all routes now compliant with next.js standards

--- src/pages/api/MongoAPI/getUserAccounts.ts
+++ src/pages/api/MongoAPI/getUserAccounts.ts
@@ -1,18 +1,15 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import clientPromise from '../../../lib/database';
+import connection from '../../../lib/database';
+import User from '../../../models/User';
 
 export default async function getUsers(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     try {
-        const client = await clientPromise;
-        const db = client.db(process.env.DATABASE_NAME);
+        await connection();
 
-        const users: object = await db
-            .collection('users')
-            .find({})
-            .toArray()
+        const users: Array<object> = await User.find();
 
         res.status(200).json(users);
     } catch (error) {
