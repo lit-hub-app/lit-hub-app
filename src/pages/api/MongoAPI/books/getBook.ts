37c424a43a5db4e71cf07b4394fd4f77746c124e
Thunderpig851 <87243196+Thunderpig851@users.noreply.github.com>
2023-03-28 12:22:52 -0400

    added a specific getBook endpoint

--- src/pages/api/MongoAPI/books/getBook.ts
+++ src/pages/api/MongoAPI/books/getBook.ts
@@ -1,9 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 import connection from '../../../../lib/database';
 import Book from '../../../../models/Book';
 import jwt from "jsonwebtoken";
 
-export default async function deleteBook(
+export default async function getBook(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
@@ -11,29 +11,29 @@ export default async function deleteBook(
         id: string;
     }
 
-    const { bookID } = req.query;
-    console.log(bookID);
+    const { id } = req.query;
 
     const token = req.headers.token as string;
     const secret = process.env.JWT_SECRET as string;
     const decoded = jwt.verify(token, secret) as JwtPayload;
 
+
     if (!token || !decoded) {
-        res.status(500).json({ message: 'Invalid token' });
+        res.status(500).json({ message: "Invalid token" });
     }
 
     try {
         await connection();
 
-        const book = await Book.findByIdAndDelete(bookID);
-        
+        const book = await Book.findById(id);
+
         if (book) {
-            res.status(200).json({success: true, book});
+            res.status(200).json({ success: true, book })
         } else {
-            res.status(204).json({success: false, message: 'Book not found'});
+            res.status(404).json({ success: false, message: "Book not found" });
         }
 
-    } catch(error) {
+    } catch (error) {
         res.status(500).json({ success: false, error });
     }
 }
